7267041e331048b8165c8f401e55a8eeb8abeff1
gavboler03 <89207920+gavboler03@users.noreply.github.com>
2026-05-28 18:39:41 -0500

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+import models
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI + SQLite working"}
