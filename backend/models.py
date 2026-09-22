a418f6b397fe95daac7595eecbc4d2e17168ddef
gavboler03 <89207920+gavboler03@users.noreply.github.com>
2026-09-22 18:54:45 -0500

    commit

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,40 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from decimal import Decimal
 from database import Base
+from sqlalchemy import ForeignKey, Numeric, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "Users"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    username: Mapped[str] = mapped_column(index=True)
-    email: Mapped[str] = mapped_column(unique=True)
-    hashed_password: Mapped[str]
+    email: Mapped[str] = mapped_column(String(250))
+
+
+class LineItem(Base):
+    __tablename__ = "line_items"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(20))
+    amount: Mapped[Decimal] = mapped_column(Numeric(8, 2))
+
+    budget_id: Mapped[int] = mapped_column(
+        ForeignKey("budgets.id")
+    )
+
+    budget: Mapped["Budget"] = relationship(
+        back_populates="line_items"
+    )
+
+
+class Budget(Base):
+    __tablename__ = "budgets"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(20))
+    description: Mapped[str] = mapped_column(String(100))
+    balance: Mapped[Decimal] = mapped_column(Numeric(8, 2))
+
+    line_items: Mapped[list["LineItem"]] = relationship(
+        back_populates="budget"
+    )
